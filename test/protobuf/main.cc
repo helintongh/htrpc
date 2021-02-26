5a6a4b0a4d7ec21771959e48b8052340681b7bdc
ubuntu <ubuntu@localhost.localdomain>
2021-02-26 22:13:44 +0800

    add protobuf example code

--- test/protobuf/main.cc
+++ test/protobuf/main.cc
@@ -2,7 +2,7 @@
  * @Description: 请输入....
  * @Author: 何林桐
  * @Date: 2020-06-01 08:35:09
- * @LastEditTime: 2020-06-01 08:40:02
+ * @LastEditTime: 2020-06-01 23:35:44
  * @LastEditors: 何林桐
  */ 
 #include "test.pb.h"
@@ -11,6 +11,32 @@
 using namespace fixbug;
 
 int main()
+{
+    // LoginResponse rsp; //声明 LoginResponse类
+    // ResultCode *rc = rsp.mutable_result(); //mutable不可改变的
+    // rc->set_errcode(1); // 指向errcode对象
+    // rc->set_errmsg("登录处理失败了");
+    
+    GetFriendListsResponse rsp; // 声明GetFriendListsResponse类
+    ResultCode *rc = rsp.mutable_result();
+    rc->set_errcode(0);
+
+    User *user1 = rsp.add_friend_list(); // 注意protobuf中变量名为friend_list,声明它为list所以自带add方法
+    user1->set_name("zhang san");
+    user1->set_age(20);
+    user1->set_sex(User::MAN);
+
+    User *user2 = rsp.add_friend_list();
+    user2->set_name("li si");
+    user2->set_age(22);
+    user2->set_sex(User::MAN);
+
+    std::cout << rsp.friend_list_size() << std::endl; // 获得好友列表的个数
+
+    return 0;
+}
+
+int main1()
 {
     // 封装了login请求对象的数据
     LoginRequest req;
